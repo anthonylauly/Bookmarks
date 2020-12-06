7e13b5a2b71f8121c23f130fb628b3ac7d9fc605
Anthony Lauly <30695180+anthonylauly@users.noreply.github.com>
2020-12-06 20:41:23 +0700

    Update settings.py

--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '#'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['mysite.com', 'localhost', '127.0.0.1', 'c281437f.ngrok.io',]
+ALLOWED_HOSTS = ['mysite.com', 'localhost', '127.0.0.1', '#',]
 
 
 # Application definition
